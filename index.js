2d55acdb20a08af7063bf140e258bc14750213a5
zaynegoering <zaynegoering@gmail.com>
2021-02-08 19:26:06 -0800

    commit tree adb4b9e28fefbe347ccb7faac3b8223a48b3bb68

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your functions here
+function map(arr, funct) {
+    let newArray = []
+    for (let e of arr) {
+        if (funct) {
+            newArray.push(funct(e))
+        } else { newArray.push(e) }
+    }
+    return newArray
+};
+
+function reduce(arr, funct, value=0) {
+    let x = value
+    for (let e of arr) {x += funct(e)}
+    return x
+};
